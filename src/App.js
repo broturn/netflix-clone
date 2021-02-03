8fa477d721a58bb998467a376a7883b760fe4b9f
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-02-02 21:22:42 -0600

    rendered posters for movies in different catergories.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
     <div className="App">
       <Row title="NETFLIX ORIGINALS" fetchURL={requests.fetchNetflixOriginals}/>
       <Row title="Treading Now" fetchURL={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchURL={requests.fetchTopRated}/>
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies}/>
+      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries}/>
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,32 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import axios from "./axios";
+import "./Row.css"
+
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchURL }) {
-    const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-    // A snippet of code which runs based on a specific condition/varaible
-    useEffect(() => {
-        // if [], run once when the row loads, and do not run again
+  // A snippet of code which runs based on a specific condition/varaible
+  useEffect(() => {
+    // if [], run once when the row loads, and do not run again
+    async function fetchData() {
+      const request = await axios.get(fetchURL);
+      setMovies(request.data.results);
+      console.log(request);
+      return request;
+    }
+    fetchData();
+  }, [fetchURL]);
 
-            async function fetchData() {
-                const request = await axios.get(fetchURL);
-                console.log(request);
-                return request;
-            }
-            fetchData();
-    }, []);
-
-    return (
-        <div className="row">
-            
-            <h2>{title}</h2>
-            {fetchURL}
-            {/* container -> posters */}
-
-        </div>
-    )
+  console.log(movies);
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Row
- 
-// 56:17
+export default Row;
