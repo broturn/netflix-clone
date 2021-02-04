e6c806232524d755774fd7c87164f5a9301f4c39
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-02-03 19:16:34 -0600

    styled the banner component

--- src/Banner.js
+++ src/Banner.js
@@ -19,6 +19,10 @@ function Banner() {
     }, [])
 
     console.log(movie);
+
+    function truncate(str, n) {
+        return str?.length > n ? str.substr(0, n-1) + "..." : str;
+    }
     return (
         <header className="banner"
         style={{
@@ -30,7 +34,7 @@ function Banner() {
         }}
         >  
             <div className="banner__contents">
-                <h1>
+                <h1 className="banner__title">
                     {movie?.title || movie?.name || movie?.original_name}
                 </h1>
                 <div className="banner__buttons">
@@ -42,7 +46,7 @@ function Banner() {
                     </button>    
                 </div>
                 <h1 className="banner__description">
-                    {movie?.overview}
+                    {truncate(movie?.overview, 150)}
                 </h1>
             </div>
             
