479b565b48a3ae99b88d65e8081d643203c7f916
Brodie Turner <64994857+broturn@users.noreply.github.com>
2021-02-04 20:55:18 -0600

    built transitions on nav component

--- src/Nav.js
+++ src/Nav.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Nav.css";
 
 function Nav() {
+  const [show, handleShow] = useState(false);
 
-    
+    useEffect(() => {
+      window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+    }, []);
 
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav__black"}`}>
       <img
         className="nav__logo"
         src="https://assets.brand.microsites.netflix.io/assets/884000e8-b375-11e7-9274-06c476b5c346_cm_1024w.png?v=21"
